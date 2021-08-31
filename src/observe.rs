f6194980e47ae2bc5480a22c1c5528098eefd88f
Collin Brittain <collinbrittain@gmail.com>
2021-08-30 23:03:59 -0500

    keepalive loop for observer subscription

--- src/observe.rs
+++ src/observe.rs
@@ -1,36 +1,59 @@
 use std::net::TcpStream;
-use tungstenite::{client, WebSocket};
+use std::sync::mpsc::{Sender};
+use std::thread;
+use tungstenite::WebSocket;
 use tungstenite::handshake::server::Response;
 use tungstenite::protocol::Message;
 use tungstenite::stream::MaybeTlsStream;
+use url::Url;
 
 pub const OBSERVER_ADDRESS: &str = "ws://observer.terra.dev";
 pub const NEW_BLOCK_SUBSCRIPTION: &str = "{ \"subscribe\": \"new_block\", \"chain_id\": \"columbus-4\" }";
 
 pub trait Subscribe {
-    fn subscribe(&mut self);
+    fn subscribe(&mut self, uri: String);
+    fn relay_messages(&mut self, sender: &Sender<String>);
 }
 
 impl Subscribe for WebSocket<MaybeTlsStream<TcpStream>> {
-    fn subscribe(&mut self) {
-        &self.write_message(Message::Text(NEW_BLOCK_SUBSCRIPTION.to_string())).unwrap();
+    fn subscribe(&mut self, uri: String) {
+        let url = Url::parse(uri.as_str()).unwrap();
+        self.write_message(Message::Text(NEW_BLOCK_SUBSCRIPTION.to_string())).unwrap();
+    }
+    
+    fn relay_messages(&mut self, sender: &Sender<String>) {
+        let msg = self.read_message().unwrap();
+        let msg: String = msg.to_string();
+        sender.send(msg).unwrap();
     }
 }
 
-pub fn get_connection() -> (WebSocket<MaybeTlsStream<TcpStream>>, Response) {
-    client::connect(OBSERVER_ADDRESS).unwrap()
+pub fn get_connection(uri: &str) -> (WebSocket<MaybeTlsStream<TcpStream>>, Response) {
+    let url = Url::parse(uri).unwrap();
+    tungstenite::connect(url).unwrap()
 }
 
-pub fn receive() -> Option<String> {
-    let mut websocket = get_connection().0;
-    websocket.subscribe();
+pub fn handle_new_block_stream(uri: String, tx: Sender<String>) {
+    // Keepalive loop
+    loop {
+        println!("Observer: Spawning new thread");
 
-    let msg = websocket.read_message().unwrap();
-    println!("{:?}", msg);
+        let sender = tx.clone();
+        let uri = uri.clone();
+        let observer_thread = thread::spawn(move || {
+            println!("Connecting to observer...");
+            let (mut websocket, _response) = get_connection(uri.as_str());
 
-    if let Message::Text(m) = msg {
-        return Some(m);
+            println!("Subscribing...");
+            websocket.subscribe(uri);
+            
+            // Listen for new_block messages and relay them over the channel
+            println!("Begin relaying");
+            loop {
+                websocket.relay_messages(&sender);
+            }
+        });
+
+        observer_thread.join().unwrap();
     }
-
-    None
 }
